6913042b2fa8efcd45b809854acad2d89a1ed9f0
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2025-11-11 21:34:08 -0600

    Ejercicio 3.4 - Generar un Array en un ciclo for

--- ejercicios.js
+++ ejercicios.js
@@ -191,6 +191,11 @@ function tablaMultiplicar(numero) {
 function numerosPares(n) {
   // TODO: Crea un array con todos los nÃºmeros pares hasta n
   const pares = [];
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 0) {
+      pares.push(i);
+    }
+  }
   return pares;
 }
 
